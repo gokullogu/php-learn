c19df7f8622f3c739664bb4c09a5ca2fbd987afc
gokullogu <gokulpollachi25@gmail.com>
2021-02-11 15:53:25 +0530

    php learn

--- data types.php
+++ data types.php
@@ -1,16 +1,39 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>data types</title>
 </head>
+
 <body>
-    <?php 
+    <?php
     //string "sequence of characters"
-    $str='hello world';
-    echo "<p>$str</p>";
+    $str = 'hello world';
+    echo "<p>string = " . $str . "</p>";
+    var_dump($str);
+    echo "<br>";
+
+    //integer "2147483648 to 2147483647"
+    $integer_num = 25;
+    echo "<p>integer = " . $integer_num . "</p>";
+    var_dump($integer_num);
+    echo "<br>";
+
+    //floating 
+    $float_num = 25.56;
+    echo "<p>floating number = " . $float_num . "</p>";
+    var_dump($float_num);
+    echo "<br>";
+
+    //boolean
+    $boolean_num = true;
+    echo "<p>boolean number = " . $boolean_num . "</p>";
+    var_dump($boolean_num);
+    echo "<br>";
     ?>
 </body>
+
 </html>
