b21d9094147977f28a924ff69b286c3ef621b119
gokullogu <gokulpollachi25@gmail.com>
2021-02-11 15:58:56 +0530

    php learn

--- data types.php
+++ data types.php
@@ -14,25 +14,25 @@
     $str = 'hello world';
     echo "<p>string = " . $str . "</p>";
     var_dump($str);
-    echo "<br>hello";
+    echo "<p>---------</p>";
 
     //integer "2147483648 to 2147483647"
     $integer_num = 25;
     echo "<p>integer = " . $integer_num . "</p>";
     var_dump($integer_num);
-    echo "<br>";
+    echo "<p>---------</p>";
 
     //floating 
     $float_num = 25.56;
     echo "<p>floating number = " . $float_num . "</p>";
     var_dump($float_num);
-    echo "<br>";
+    echo "<p>---------</p>";
 
     //boolean
     $boolean_num = true;
     echo "<p>boolean number = " . $boolean_num . "</p>";
     var_dump($boolean_num);
-    echo "<br>";
+    echo "<p>---------</p>";
     ?>
 </body>
 
