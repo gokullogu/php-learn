64855d7769181fbd5d1459a5bb4bc237a0b75b62
gokullogu <gokulpollachi25@gmail.com>
2021-02-13 21:45:29 +0530

    while

---
+++ tutorial/while.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>loop</title>
+</head>
+<body>
+    <?PHP
+      $num1=0;
+      while($num1<5){
+          $num1++;
+          echo "<p>num+1 = $num1</p>";
+      }
+    ?>
+</body>
+</html>
